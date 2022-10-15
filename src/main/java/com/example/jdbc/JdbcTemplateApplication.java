4f2707f81da5360920e853dea5a69e91b8f77599
YasiruLak <yasirudahanayaka19@gmail.com>
2022-10-15 18:35:53 +0530

    Configure Projects

---
+++ src/main/java/com/example/jdbc/JdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcTemplateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/jdbc/model/Course.java
@@ -0,0 +1,6 @@
+package com.example.jdbc.model;
+
+public class Course {
+
+    
+}
